d6472680564d5274b87272cfedc826861b6c7ed9
Juan Antonio Piñero <japinero@area-integral.com>
2016-03-21 12:07:34 +0100

    Added project cli command

--- lib/tzispa/cli.rb
+++ lib/tzispa/cli.rb
@@ -1,4 +1,5 @@
 require 'thor'
+require 'tzispa/cli/project'
 
 module Tzispa
 
@@ -10,12 +11,19 @@ module Tzispa
       puts "v#{Tzispa::VERSION}"
     end
 
-    desc 'new', 'Generate Tzispa project components'
+    desc 'project', 'Generate Tzispa project components'
+    def project(name)
+      Tzispa::CommandCli::Project.new.create(name)
+      puts "created new project #{name}"
+    end
+
+    desc 'app', 'Generate new application into a project'
     method_option :domain, :aliases => "-d", :desc => "The app domain name", :required => true
     method_option :mount, :aliases => "-m", :desc => "The mount point for this app", :required => true
     method_option :host, :aliases => "-h", :desc => "The hostname used for this app ", :required => true
-    def new(name)
-      puts "creating new #{name}"
+    def app(name)
+      Tzispa::CommandCli::App.new.create name, options[:domain], options[:mount]
+      puts "created new app #{name}"
     end
 
   end
