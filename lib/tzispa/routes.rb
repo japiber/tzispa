b9c5bab220dbac89b288ab7a9e24f459e34c0c79
Juan Antonio Piñero <japinero@area-integral.com>
2016-05-24 09:19:23 +0200

    Set router default app to http_error 404

--- lib/tzispa/routes.rb
+++ lib/tzispa/routes.rb
@@ -3,6 +3,7 @@
 require 'yaml'
 require 'http_router'
 require 'tzispa/utils/string'
+require 'tzispa/controller/http_error'
 
 module Tzispa
 
@@ -14,6 +15,7 @@ module Tzispa
 
     def initialize(map_path=nil)
       @router = HttpRouter.new
+      @router.default Tzispa::Controller::HttpError.new('error_404')
       @map_path = map_path unless map_path=='/'
     end
 
@@ -21,33 +23,34 @@ module Tzispa
       "#{@map_path}#{@router.path path_id, params}"
     end
 
-    def add(route_id, path, controller, methods)
+    def add(route_id, path, methods, controller)
       spec_control, callmethod = controller.to_s.split(':')
       mpath = spec_control.split('#')
-      controller = TzString.camelize(mpath.pop).to_s
+      req_controller = mpath.pop
+      controller = TzString.camelize(req_controller).to_s
       if mpath.count > 1
         controller_module = mpath.collect!{ |w| w.capitalize }.join('::')
-        require_relative "./controller/#{controller.downcase}"
+        require_relative "./controller/#{req_controller}"
       else
         controller_module = CONTROLLERS_BASE
-        require "tzispa/controller/#{controller.downcase}"
+        require "tzispa/controller/#{req_controller}"
       end
-      @router.add(path, {request_method: methods}).tap { |rule|
+      @router.add(path, methods ? {request_method: methods} : nil ).tap { |rule|
         rule.to TzString.constantize("#{controller_module}::#{controller}").new(callmethod)
         rule.name = route_id
       }
     end
 
-    def index(path, methods, controller=nil)
-      add :index, path, controller || 'layout:render!', methods
+    def index(path, methods=nil, controller=nil)
+      add :index, path, methods, controller || 'layout:render!'
     end
 
-    def api(path, methods, controller=nil)
-      add :api, path, controller || 'api:dispatch!', methods
+    def api(path, methods=nil, controller=nil)
+      add :api, path, methods, controller || 'api:dispatch!'
     end
 
-    def site(path, methods, controller=nil)
-      add :site, path, controller || 'layout:render!', methods
+    def site(path, methods=nil, controller=nil)
+      add :site, path, methods, controller || 'layout:render!'
     end
 
   end
