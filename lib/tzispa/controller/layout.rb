abcbb9aef97fa502c46739aa0002d8389c18f53e
Juan Antonio Piñero <japinero@area-integral.com>
2016-06-03 20:19:52 +0200

    remove controller/base custom headers

--- lib/tzispa/controller/layout.rb
+++ lib/tzispa/controller/layout.rb
@@ -16,29 +16,12 @@ module Tzispa
         rig = context.app.engine.layout(name: layout_name, format: layout_format.to_sym)
         response.body << rig.render(context)
         content_type layout_format
-        set_layout_headers
       end
 
       private
 
       def layout_name
-        if config.auth_required && !context.logged? && context.layout
-          config.default_layout
-        else
-          context.layout || config.default_layout
-        end
-      end
-
-
-      def set_layout_headers
-        headers = Hash.new
-        if config.cache.layout.enabled
-          headers['Cache-Control'] = config.cache.layout.control
-          if config.cache.layout.expires
-            headers['Expires'] = (Time.now + config.cache.layout.expires).utc.rfc2822
-          end
-        end
-        response.headers.merge!(headers)
+        context.layout || config.default_layout
       end
 
 
