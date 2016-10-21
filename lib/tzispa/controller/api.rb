aa5daeb84dd0e029fb8154b9c88a82c5e3743144
Juan Antonio Piñero <japinero@area-integral.com>
2016-10-21 08:19:39 +0200

    handler calling improvements

--- lib/tzispa/controller/api.rb
+++ lib/tzispa/controller/api.rb
@@ -10,48 +10,51 @@ require 'tzispa/utils/string'
 
 module Tzispa
   module Controller
+
+    class ControllerException < StandardError; end
+
     class Api < Base
 
       include Tzispa::Helpers::Response
 
-      attr_reader :hnd
+      attr_reader :handler
 
       def self.generate_handler(domain, name)
         @domain = domain
-        @handler = name
+        @handler_name = name
         raise "The handler '#{name}' already exist" if File.exist?(handler_class_file)
         File.open(handler_class_file, "w") { |f|
-          hnd_code = TzString.new
-          f.puts hnd_code.indenter("require 'tzispa/api/handler'\n\n")
+          handler_code = TzString.new
+          f.puts handler_code.indenter("require 'tzispa/api/handler'\n\n")
           level = 0
           handler_namespace.split('::').each { |ns|
-            f.puts hnd_code.indenter("module #{ns}\n", level > 0 ? 2 : 0).to_s
+            f.puts handler_code.indenter("module #{ns}\n", level > 0 ? 2 : 0).to_s
             level += 1
           }
-          f.puts hnd_code.indenter("\nclass #{handler_class_name} < Tzispa::Api::Handler\n\n", 2)
-          f.puts hnd_code.indenter("end\n\n")
+          f.puts handler_code.indenter("\nclass #{handler_class_name} < Tzispa::Api::Handler\n\n", 2)
+          f.puts handler_code.indenter("end\n\n")
           handler_namespace.split('::').each { |ns|
-            f.puts hnd_code.unindenter("end\n", 2)
+            f.puts handler_code.unindenter("end\n", 2)
           }
         }
       end
 
 
       def dispatch!
-        @handler, domain_name = context.router_params[:handler].split('.').reverse
+        @handler_name, domain_name = context.router_params[:handler].split('.').reverse
         @domain = domain_name.nil? ? context.app.domain : Tzispa::Domain.new(name: domain_name)
         @verb = context.router_params[:verb]
         @predicate = context.router_params[:predicate]
-        @hnd = handler_class.new(context)
-        @predicate ? hnd.send(@verb, @predicate) : hnd.send(@verb)
-        context.flash << hnd.message
-        send hnd.response_verb if hnd.response_verb
+        @handler = handler_class.new(context)
+        handler.send @verb, @predicate
+        context.flash << handler.message
+        send handler.response_verb if handler.response_verb
         response.finish
       end
 
       def redirect
-        url = if hnd.data && !hnd.data.strip.empty?
-          hnd.data.start_with?('#') ? "#{request.referer}#{hnd.data}" : hnd.data
+        url = if handler.data && !handler.data.strip.empty?
+          handler.data.start_with?('#') ? "#{request.referer}#{handler.data}" : handler.data
         else
           request.referer
         end
@@ -59,18 +62,18 @@ module Tzispa
       end
 
       def html
-        response.body << hnd.data
+        response.body << handler.data
         content_type :htm
         set_action_headers
       end
 
       def json
-        if hnd.data.is_a?(::Hash)
-          data = hnd.data
-          data[:__result_status] = hnd.status
-          data[:__result_message] = hnd.message
+        if handler.data.is_a?(::Hash)
+          data = handler.data
+          data[:__result_status] = handler.status
+          data[:__result_message] = handler.message
         else
-          data = JSON.parse(hnd.data)
+          data = JSON.parse(handler.data)
         end
         response.body << data.to_json
         content_type :json
@@ -78,21 +81,21 @@ module Tzispa
       end
 
       def text
-        response.body << hnd.data
+        response.body << handler.data
         content_type :text
         set_action_headers
       end
 
       def download
-        send_file hnd.data[:path], hnd.data
+        send_file handler.data[:path], handler.data
       end
 
       def handler_class_name
-        "#{TzString.camelize @handler}Handler"
+        "#{TzString.camelize @handler_name}Handler"
       end
 
       def handler_class_file
-        "#{@domain.path}/api/#{@handler}.rb"
+        "#{@domain.path}/api/#{@handler_name}.rb"
       end
 
       def handler_namespace
@@ -100,7 +103,7 @@ module Tzispa
       end
 
       def handler_class
-        @domain.require "api/#{@handler}"
+        @domain.require "api/#{@handler_name}"
         TzString.constantize "#{handler_namespace}::#{handler_class_name}"
       end
 
@@ -110,7 +113,7 @@ module Tzispa
 
       def set_action_headers
         response['X-API'] = "#{context.router_params[:sign]}:#{context.router_params[:handler]}:#{context.router_params[:verb]}:#{context.router_params[:predicate]}"
-        response['X-API-STATE'] = "#{hnd.status}"
+        response['X-API-STATE'] = "#{handler.status}"
       end
 
     end
