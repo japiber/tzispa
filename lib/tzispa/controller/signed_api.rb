fc630f63866763135c20992031fd2ba04c55a91d
Juan Antonio Piñero <japinero@area-integral.com>
2016-10-28 15:52:51 +0200

    deprecated controller, api sign checking has been moved to handlers by dsl

--- lib/tzispa/controller/signed_api.rb
+++ lib/tzispa/controller/signed_api.rb
@@ -5,25 +5,8 @@ require 'tzispa/helpers/security'
 module Tzispa
   module Controller
     class SignedApi < Api
-
-      include Tzispa::Helpers::Security
-
-
-      def dispatch!
-        raise Error::InvalidSign.new unless sign?
-        super        
-      end
-
-      private
-
-      def sign?
-        context.router_params[:sign] == sign_array([
-           context.router_params[:handler],
-           context.router_params[:verb],
-           context.router_params[:predicate]
-          ],
-          context.app.config.salt)
-      end
+      
+      # deprecated !!
 
     end
   end
