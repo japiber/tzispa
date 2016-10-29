1aaf3989e4929d7ecfd06400fc33545e0892471f
Juan Antonio Piñero <japinero@area-integral.com>
2016-10-29 19:10:27 +0200

    use provider and sign_requirer modules to expose better dsl

--- lib/tzispa/api/handler.rb
+++ lib/tzispa/api/handler.rb
@@ -2,6 +2,8 @@
 
 require 'forwardable'
 require 'json'
+require 'tzispa/helpers/provider'
+require 'tzispa/helpers/sign_requirer'
 
 module Tzispa
   module Api
@@ -15,11 +17,12 @@ module Tzispa
     class InvalidSign < ApiException; end
 
     class Handler
+      include Tzispa::Helpers::Provider
+      include Tzispa::Helpers::SignRequirer
       extend Forwardable
 
-      def_delegators :@context, :request, :response, :app
-
       attr_reader :context, :status, :response_verb, :data
+      def_delegators :@context, :request, :response, :app, :repository
 
       HANDLED_UNDEFINED         = nil
       HANDLED_OK                = 1
@@ -63,60 +66,21 @@ module Tzispa
       end
 
       def call(verb, predicate=nil)
-        raise UnknownHandlerVerb.new(verb, self.class.name) unless self.class.provides? verb
-        raise InvalidSign.new if self.class.sign_required? && !sign?
+        raise UnknownHandlerVerb.new(verb, self.class.name) unless provides? verb
+        raise InvalidSign.new if sign_required? && !sign_valid?
         # allow compound predicates
         args = predicate ? predicate.split(',') : nil
         send verb, *args
       end
 
-      class << self
 
-        def provides(*args)
-          (@provides ||= Hash.new).tap { |prv|
-            args&.each { |s|
-              prv[s.to_sym] = s
-            }
-          }
-        end
+      protected
 
-        def provides?(verb)
-          value = verb.to_sym
-          provides.include?(value) && public_method_defined?(provides[value])
-        end
-
-        def mapping(source, dest)
-          provides[source] = dest
-        end
-
-        def required(target, *args, &block)
-          @required ||= Hash.new
-          (@required[target] ||= Hash.new).tap { |reqt|
-            args&.each { |s|
-              reqt[s] = block || true
-            }
-          }
-        end
-
-        def required?(target, value)
-          required(target)&.fetch(value, nil)
-        end
-
-        def sign_required!
-          required :router_params, :sign
-        end
-
-        def sign_required?
-          required? :router_params, :sign
-        end
-
+      def static_path_sign?
+        context.path_sign? context.router_params[:sign], context.router_params[:handler], context.router_params[:verb], context.router_params[:predicate]
       end
 
       private
-
-      def sign?
-        context.path_sign? context.router_params[:sign], context.router_params[:handler], context.router_params[:verb], context.router_params[:predicate]
-      end
 
       def result_messages(status)
         self.class::RESULT_MESSAGES[status] if (defined?( self.class::RESULT_MESSAGES ) && self.class::RESULT_MESSAGES.is_a?(Hash))
