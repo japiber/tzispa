0a58b51c86bdb0b32465fa70c28499b603c05294
Juan Antonio Piñero <japinero@area-integral.com>
2017-02-23 11:06:05 +0100

    use before handler for user login check

--- lib/tzispa/controller/auth_layout.rb
+++ lib/tzispa/controller/auth_layout.rb
@@ -1,35 +1,16 @@
 # frozen_string_literal: true
 
-require 'tzispa_rig'
-require 'tzispa/controller/base'
-require 'tzispa/controller/exceptions'
-require 'tzispa/helpers/response'
-require 'tzispa_rig'
+require 'forwardable'
+require 'tzispa/controller/layout'
+
 
 module Tzispa
   module Controller
-    class AuthLayout < Base
-      include Tzispa::Helpers::Response
+    class AuthLayout < Layout
 
-      def render!
-        if (layout_name == login_layout) || context.logged?
-          rig = Tzispa::Rig::Engine.layout name: layout_name, domain: application.domain, content_type: context.router_params[:format] || config.default_format
-          response.body << rig.render(context)
-          content_type rig.content_type
-        else
-          context.redirect login_layout, true, response
-        end
-      end
+      def_delegators :context, :login_redirect
 
-      private
-
-      def layout_name
-        context.layout || config.default_layout
-      end
-
-      def login_layout
-        config.login_layout
-      end
+      before :login_redirect
 
 
     end
