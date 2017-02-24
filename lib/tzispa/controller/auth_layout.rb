c672bd6c714aaacadceaadb926ba8def231e3134
Juan Antonio Piñero <japinero@area-integral.com>
2017-02-24 10:54:49 +0100

    remove unneeded delegator

--- lib/tzispa/controller/auth_layout.rb
+++ lib/tzispa/controller/auth_layout.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
-require 'forwardable'
 require 'tzispa/controller/layout'
 
 module Tzispa
   module Controller
     class AuthLayout < Layout
 
-      def_delegators :context, :login_redirect
-
       before :login_redirect
-
 
     end
   end
