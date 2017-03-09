433e68ad62dff36962d5543ad0aa8b2aff0225d7
Juan Antonio Piñero <japinero@area-integral.com>
2017-03-09 09:34:10 +0100

    better error handleing in invoke

--- lib/tzispa/controller/base.rb
+++ lib/tzispa/controller/base.rb
@@ -13,7 +13,7 @@ module Tzispa
 
       include Tzispa::Helpers::ErrorView
 
-      attr_reader :context, :application
+      attr_reader :context, :application, :callmethod
       def_delegators :@context, :request, :response, :config,
                      :login_redirect, :unauthorized_but_logged
 
@@ -24,7 +24,7 @@ module Tzispa
 
       def call(env)
         @context = Tzispa::Http::Context.new(@application, env)
-        invoke if @callmethod
+        invoke if callmethod
         response.finish
       end
 
@@ -43,31 +43,53 @@ module Tzispa
 
       def invoke
         prepare_response catch(:halt) {
-          begin
-            do_before
-            send @callmethod
-          rescue Tzispa::Rig::NotFound => ex
-            context.logger.info "#{ex.message} (#{ex.class})"
-            404
-          rescue StandardError, ScriptError, SecurityError => ex
-            context.logger.error error_log(ex)
-            @dinfo = debug_info(ex) if Tzispa::Environment.development?
-            500
-          end
+          do_before
+          send callmethod
         }
+      rescue Tzispa::Rig::NotFound => ex
+        prepare_client_error(404, ex)
+      rescue StandardError, ScriptError, SecurityError => ex
+        prepare_server_error(500, ex)
       end
 
-      def prepare_response(status)
+      def prepare_response(status, content = nil)
         response.status = status if status.is_a?(Integer)
         if response.client_error?
-          response.body = error_page(context.domain, status: response.status)
+          prepare_client_error(status)
         elsif response.server_error?
-          response.body = @dinfo || error_page(context.domain, status: response.status)
+          prepare_server_error(status)
+        elsif content
+          response.body = content
         end
       end
 
       def do_before
-        self.class.before.each { |hbef| send hbef }
+        self.class.before.each { |hook| send hook }
+      end
+
+      def prepare_client_error(status, error = nil)
+        status.tap do |code|
+          context.logger.info log_format(code, error.to_s) if error
+          response.body = error_page(context.domain, status: code)
+        end
+      end
+
+      def prepare_server_error(status, error = nil)
+        status.tap do |code|
+          context.logger.error log_format(code, error_log(error)) if error
+          response.body = if error && Tzispa::Environment.development?
+                            debug_info(error)
+                          else
+                            error_page(context.domain, status: code)
+                          end
+        end
+      end
+
+      def log_format(status, msg)
+        String.new.tap do |str|
+          str << "[#{context.request.ip} #{DateTime.now}] #{context.request.request_method}"
+          str << " #{context.request.fullpath} #{status}\n#{msg}"
+        end
       end
     end
 
