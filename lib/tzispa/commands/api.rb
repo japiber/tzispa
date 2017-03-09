9f3868365bef5ce5814342a5a523b649f81731b5
Juan Antonio Piñero <japinero@area-integral.com>
2017-03-09 09:33:24 +0100

    updates to generate api file command

--- lib/tzispa/commands/api.rb
+++ lib/tzispa/commands/api.rb
@@ -16,7 +16,33 @@ module Tzispa
       end
 
       def generate
-        Tzispa::Controller::Api.new.generate_handler(domain, name)
+        generate_handler(domain, name, request_method)
+      end
+
+      private
+
+      def generate_handler(domain, name, request_method)
+        class_name = Tzispa::Api.handler_class_name(name)
+        file_name = Tzispa::Api.handler_class_file(domain, name, request_method)
+        raise "The handler '#{name}' already exist" if File.exist?(file_name)
+        namespace = Tzispa::Api.handler_namespace(domain, request_method)
+        File.open(file_name, 'w') do |f|
+          f.puts handler_code(namespace, class_name)
+        end
+      end
+
+      def handler_code(namespace, class_name)
+        String.new.tap do |code|
+          code.indenter("require 'tzispa/api/handler'\n\n")
+          level = 0
+          namespace.split('::').each do |ns|
+            code.indenter("module #{ns}\n", level.positive? ? 2 : 0).to_s
+            level += 1
+          end
+          code.indenter("\nclass #{class_name} < Tzispa::Api::Handler\n\n", 2)
+          code.indenter("end\n\n")
+          namespace.split('::').each { code.unindenter("end\n", 2) }
+        end
       end
     end
 
