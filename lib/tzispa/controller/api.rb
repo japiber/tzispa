1e053c8101ca16ec4411d7301aa8e44ad892b806
Juan Antonio Piñero <japinero@area-integral.com>
2017-03-09 09:35:36 +0100

    better api responses by content type

--- lib/tzispa/controller/api.rb
+++ lib/tzispa/controller/api.rb
@@ -22,7 +22,7 @@ module Tzispa
         predicate = context.router_params[:predicate]
         handler = prepare_handler
         handler.run! verb, predicate
-        send(handler.response_verb, handler) if handler.response_verb
+        send(handler.type, handler) if handler.type
         response.finish
       end
 
@@ -39,45 +39,52 @@ module Tzispa
         context.router_params[:handler].split('.').last
       end
 
+      def handler_redirect_url(url)
+        if url && !url.strip.empty?
+          url.start_with?('#') ? "#{request.referer}#{url}" : url
+        else
+          request.referer
+        end
+      end
+
       def redirect(handler)
-        url = if handler.data && !handler.data.strip.empty?
-                handler.data.start_with?('#') ? "#{request.referer}#{handler.data}" : handler.data
-              else
-                request.referer
-              end
-        context.flash << handler.message if config.sessions&.enabled && handler.error?
-        context.redirect url, config.absolute_redirects, response
+        api_flash(handler.message) if handler.error?
+        context.redirect handler.redirect_url(handler.data), config.absolute_redirects, response
       end
 
       def html(handler)
-        content_type :htm
-        context.flash << handler.message if config.sessions&.enabled && handler.error?
-        response.body << handler.data
-        set_api_headers handler.status
+        content = handler.error? ? handler.message : handler.data
+        api_response :htm, content, handler.status, handler.error
       end
 
       def json(handler)
-        content_type :json
-        data = handler.data.is_a?(::String) ? JSON.parse(handler.data) : handler.data.to_json
-        response.body << if handler.error?
-                           Hash[:__error, true,
-                                :__error_msg, handler.message,
-                                :__error_code, handler.status].to_json
-                         else
-                           data
-                         end
-        set_api_headers handler.status
+        content = if handler.error?
+                    { error_message: handler.message,
+                      error_code: handler.error }.to_json
+                  else
+                    handler.data.is_a?(::String) ? JSON.parse(handler.data) : handler.data.to_json
+                  end
+        api_response :json, content, handler.status, handler.error
       end
 
       def text(handler)
-        content_type :text
-        context.flash << handler.message if config.sessions&.enabled && handler.error?
-        response.body << handler.data
-        api_headers handler.status
+        content = handler.error? ? handler.message : handler.data
+        api_response :text, content, handler.status, handler.error
       end
 
       def download(handler)
         send_file handler.data[:path], handler.data
+      end
+
+      def api_flash(message)
+        context.flash << message if config.sessions&.enabled
+      end
+
+      def api_response(type, content, status = nil, error = nil)
+        content_type type
+        response.body << content
+        response.status = status if status
+        api_headers error
       end
 
       def request_method
@@ -89,8 +96,8 @@ module Tzispa
           "#{handler_name.camelize}Handler"
         end
 
-        def handler_class_file(domain, handler_name)
-          "#{domain.path}/api/#{handler_name}.rb"
+        def handler_class_file(domain, handler_name, request_method)
+          "#{domain.path}/api/#{request_method}/#{handler_name}.rb"
         end
 
         def handler_namespace(domain, request_method)
@@ -102,36 +109,18 @@ module Tzispa
           "#{handler_namespace domain, request_method}::#{handler_class_name handler_name}"
             .constantize
         end
-
-        def generate_handler(domain, name)
-          raise "The handler '#{name}' already exist" if File.exist?(handler_class_file)
-          File.open(handler_class_file(domain, name), 'w') do |f|
-            handler_code = String.new
-            f.puts handler_code.indenter("require 'tzispa/api/handler'\n\n")
-            level = 0
-            handler_namespace.split('::').each do |ns|
-              f.puts handler_code.indenter("module #{ns}\n", level.positive? ? 2 : 0).to_s
-              level += 1
-            end
-            f.puts handler_code.indenter("\nclass #{handler_class_name} < Tzispa::Api::Handler\n\n", 2)
-            f.puts handler_code.indenter("end\n\n")
-            handler_namespace.split('::').each do |ns|
-              f.puts handler_code.unindenter("end\n", 2)
-            end
-          end
-        end
       end
 
       private
 
-      def api_headers(status)
+      def api_headers(error = nil)
         handler = context.router_params[:handler]
         verb = context.router_params[:verb]
         predicate = context.router_params[:predicate]
         response['X-API'] = "#{handler}:#{verb}:#{predicate}"
-        response['X-API-STATE'] = status&.to_s
+        response['X-API-SR'] = error.to_s if error
       end
-
     end
+
   end
 end
