389cea5b720faa4f581803ae5a5f790ce85fca49
Juan Antonio Piñero <japinero@area-integral.com>
2017-05-05 10:33:29 +0200

    fix errors in rig templates creation

--- lib/tzispa/commands/rig.rb
+++ lib/tzispa/commands/rig.rb
@@ -8,19 +8,18 @@ module Tzispa
     class Rig < Command
       attr_reader :name, :domain, :type, :mime_format
 
-      def initialize(name, app, type, mime_format = nil, options = nil)
+      def initialize(name, app, type, options = nil)
         super(options)
         @domain = Tzispa::Domain.new app
         @type = type.to_sym
         @name = name
-        @mime_format = mime_format
       end
 
       def generate
         Tzispa::Rig::Template.new(name: name,
                                   type: type,
                                   domain: domain,
-                                  format: mime_format).create
+                                  content_type: :htm).create
       end
     end
 
