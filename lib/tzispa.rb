f40fd672805f4988a0af3996fadf64b41b691b03
Juan Antonio Piñero <japinero@area-integral.com>
2017-10-13 19:07:16 +0200

    autoload some class constants

--- lib/tzispa.rb
+++ lib/tzispa.rb
@@ -1,8 +1,16 @@
 module Tzispa
 
   require 'tzispa/version'
-  require 'tzispa/environment'
-  require 'tzispa/server'
-  require 'tzispa/app'
+
+  autoload :Environment,    'tzispa/environment'
+  autoload :Server,         'tzispa/server'
+  autoload :Application,    'tzispa/app'
+  autoload :Cli,            'tzispa/cli'
+  autoload :Context,        'tzispa/context'
+  autoload :Domain,         'tzispa/domain'
+  autoload :Env,            'tzispa/env'
+  autoload :RouteSet,       'tzispa/route_set'
+  autoload :Server,         'tzispa/server'
+  autoload :Tzisparc,       'tzispa/tzisparc'
 
 end
